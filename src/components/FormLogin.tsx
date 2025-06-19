f4b03296137fc1b478f8b64bfc0d85c0465c2f45
Caio1213 <caiofxpgm@gmail.com>
2025-06-19 17:42:36 -0300

    feat: formLogin

---
+++ src/components/FormLogin.tsx
@@ -0,0 +1,47 @@
+import { useForm } from "react-hook-form";
+import { DivGroupInput, Input } from "./GruopInput";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import ButtonEye from "./ButtonEye";
+import ButtonSubmit from "./ButtonSubmit";
+const schema = z.object({
+  email: z.string().email("Email inválido"),
+  password: z.string().min(6, "Senha deve ter pelo menos 6 caracteres"),
+});
+
+type FormLoginData = z.infer<typeof schema>;
+
+function FormLogin() {
+  const [type, setType] = useState<boolean>(true);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormLoginData>({
+    resolver: zodResolver(schema),
+  });
+
+  function sendData(data: FormLoginData) {
+    console.log(data);
+  }
+
+  return (
+    <form
+      className="flex justify-center flex-col items-center gap-4 bg-gray-700 py-10 px-5 rounded-2xl border-2 border-gray-600"
+      onSubmit={handleSubmit(sendData)}
+    >
+      <DivGroupInput title="Email" messageError={errors.email?.message}>
+        <Input type="text" {...register("email")} />
+      </DivGroupInput>
+
+      <DivGroupInput title="Senha" messageError={errors.password?.message}>
+        <Input type={type ? "password" : "text"} {...register("password")} />
+        <ButtonEye type={type} setType={setType} />
+      </DivGroupInput>
+
+      <ButtonSubmit />
+    </form>
+  );
+}
+export default FormLogin;
