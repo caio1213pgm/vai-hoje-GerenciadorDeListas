ce218375dea9c823497f7a3fe6d3d30579b5dcec
Caio1213 <caiofxpgm@gmail.com>
2025-06-19 15:37:21 -0300

    feat: home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router";
+import "../App.css";
+import GroupCardsApresentation from "../components/GroupCardsApresentation";
+import Layout from "../layout/Layout";
+
+function App() {
+  return (
+    <Layout>
+      <GroupCardsApresentation/>
+      <div className="flex justify-center">
+        <button className="bg-gray-700 px-8 py-4 text-xl text-white font-medium rounded-xl hover:scale-110 cursor-pointer duration-200">
+          <Link to="/login">
+          Comece agora a criar sua lista
+          </Link>
+        </button>
+
+      </div>
+    </Layout>
+  );
+}
+
+export default App;
