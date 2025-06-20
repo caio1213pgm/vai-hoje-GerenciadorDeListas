ef76dded0f430072982b4e2f52c0bdefdf5a1832
Caio1213 <caiofxpgm@gmail.com>
2025-06-20 14:28:15 -0300

    feat: edit home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,14 +6,14 @@ import Layout from "../layout/Layout";
 function App() {
   return (
     <Layout>
-      <GroupCardsApresentation/>
-      <div className="flex justify-center">
-        <button className="bg-gray-700 px-8 py-4 text-xl text-white font-medium rounded-xl hover:scale-110 cursor-pointer duration-200">
-          <Link to="/login">
-          Comece agora a criar sua lista
-          </Link>
-        </button>
+      <GroupCardsApresentation />
 
+      <div className="flex justify-center">
+        <Link to="/login">
+          <button className="bg-gray-700 px-8 py-4 text-xl text-white font-medium rounded-xl hover:scale-110 cursor-pointer duration-200">
+            Comece agora a criar sua lista
+          </button>
+        </Link>
       </div>
     </Layout>
   );
