acbc23301d0d47870856435b67adf47b5f880bf1
Caio1213 <caiofxpgm@gmail.com>
2025-06-20 14:25:57 -0300

    fix: edit button, add text dinamic

--- src/components/ButtonSubmit.tsx
+++ src/components/ButtonSubmit.tsx
@@ -1,10 +1,13 @@
-function ButtonSubmit() {
+type ButtonSubmitProps = {
+  text: string;
+}
+function ButtonSubmit({text}: ButtonSubmitProps) {
   return (
     <button
       type="submit"
-      className="bg-gray-800 text-white text-xl px-7 py-1 rounded border-2 border-gray-600 hover:bg-gray-600 transition-colors"
+      className="bg-gray-800 text-white text-xl px-7 py-1 rounded border-2 border-gray-600 hover:bg-gray-600 transition-colors cursor-pointer"
     >
-      Entrar
+      {text}
     </button>
   );
 }
