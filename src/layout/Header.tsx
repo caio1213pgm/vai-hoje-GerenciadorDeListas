dfd98547fb5966646c3647a57c83e4e1af79eafc
Caio1213 <caiofxpgm@gmail.com>
2025-06-25 16:08:41 -0300

    feat: add User component to header to display user information

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,12 +1,32 @@
 import { Link } from "react-router";
+import { type User } from "firebase/auth";
+import { FaUserSlash } from "react-icons/fa6";
+import { FaUser } from "react-icons/fa";
+import { useAuth } from "../context/AuthContext";
+
+function User() {
+  const { user } = useAuth();
+
+  return (
+    <div className="flex items-center text-white gap-2 pr-6">
+      <h1 className="text-white text-lg">
+        {user ? `Olá, ${user.displayName}!` : "Você não está logado"}
+      </h1>
+      {user ? <FaUser /> : <FaUserSlash />}
+    </div>
+  );
+}
 
 function Header() {
   return (
     <header className="bg-gray-700 h-[82px]">
-      <div className="flex items-center h-full ">
+      <div className="flex items-center h-full justify-between">
         <h1 className="text-3xl font-medium text-white hover:scale-105 pl-6 cursor-pointer">
           <Link to="/">Vai Hoje?</Link>
         </h1>
+        <div>
+          <User />
+        </div>
       </div>
     </header>
   );
