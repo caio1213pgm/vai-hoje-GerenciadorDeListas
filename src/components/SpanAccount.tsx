ca7100c3e192e53079d3c70badadfc260d0ea9b3
Caio1213 <caiofxpgm@gmail.com>
2025-06-25 18:05:14 -0300

    fix: add text color to SpanAccount component for better visibility

--- src/components/SpanAccount.tsx
+++ src/components/SpanAccount.tsx
@@ -4,7 +4,7 @@ type SpanAccountProps = {
 };
 function SpanAccount({ title, informations }: SpanAccountProps) {
   return (
-    <span className="flex gap-1 items-end">
+    <span className="flex gap-1 items-end text-gray-300">
       <p className="md:text-xl font-medium">{title}:</p>
       {informations}
     </span>
