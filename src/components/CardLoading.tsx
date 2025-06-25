097da49220d1d65292453ab1b2656bcbe016b5c2
Caio1213 <caiofxpgm@gmail.com>
2025-06-25 16:09:28 -0300

    feat: add CardLoading component to loading view

---
+++ src/components/CardLoading.tsx
@@ -0,0 +1,10 @@
+function CardLoading() {
+  return (
+    <div className="flex gap-4 p-4 items-center justify-center my-auto">
+      <div className="py-4 bg-gray-300 animate-pulse rounded-lg w-1/4 text-black text-2xl font-medium text-center">
+        <p>Carregando...</p>
+      </div>
+    </div>
+  );
+}
+export default CardLoading;
