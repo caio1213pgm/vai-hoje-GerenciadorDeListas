d2c4ba1606fb21fbd39cbb7ac562b41bd253b166
Caio1213 <caiofxpgm@gmail.com>
2025-06-25 16:09:08 -0300

    feat: add AuthProvider and conditional routes to user profile

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 
 import { AppRoutes } from "./routes/AppRoutes.tsx";
+import AuthProvider from "./context/AuthContext.tsx";
 
-createRoot(document.getElementById("root")!).render(<AppRoutes />);
+createRoot(document.getElementById("root")!).render(
+  <AuthProvider>
+    <AppRoutes />
+  </AuthProvider>
+);
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,14 +2,22 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import App from "../pages/Home";
 import Login from "../pages/auth/Login";
 import Register from "../pages/auth/Register";
+import Perfil from "../pages/auth/Perfil";
+import { useAuth } from "../context/AuthContext";
 
 export function AppRoutes() {
+  const { user } = useAuth();
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        {user ? (
+          <Route path="/perfil" element={<Perfil />} />
+        ) : (
+          <Route path="/perfil" element={<Login />} />
+        )}
       </Routes>
     </BrowserRouter>
   );
