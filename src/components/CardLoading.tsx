26b9ec1ae842fd9a3d2ff18d3cd7824269e5df8a
Caio1213 <caiofxpgm@gmail.com>
2025-07-04 17:32:29 -0300

    feat: add horizontal padding to CardLoading component

--- src/components/CardLoading.tsx
+++ src/components/CardLoading.tsx
@@ -1,7 +1,7 @@
 function CardLoading() {
   return (
     <div className="flex gap-4 p-4 items-center justify-center my-auto">
-      <div className="py-4 bg-gray-300 animate-pulse rounded-lg w-1/4 text-black text-2xl font-medium text-center">
+      <div className="py-4 px-4 bg-gray-300 animate-pulse rounded-lg md:w-1/4 text-black text-2xl font-medium text-center">
         <p>Carregando...</p>
       </div>
     </div>
