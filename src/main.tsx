5edfcae898f41fe4d0de4296d29db79452f5eb22
caio1213pgm <caiofxpgm@gmail.com>
2025-07-15 18:20:06 -0300

    refactor: remove DashboardProvider from main.tsx to simplify component structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { AppRoutes } from "./routes/AppRoutes.tsx";
 import AuthProvider from "./context/AuthContext.tsx";
-import DashboardProvider from "./context/DashboardContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <AuthProvider>
-    <DashboardProvider>
-      <AppRoutes />
-    </DashboardProvider>
-  </AuthProvider>
+    <AuthProvider>
+        <AppRoutes />
+    </AuthProvider>
 );
