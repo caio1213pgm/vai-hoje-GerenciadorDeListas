77396a6b4d752c235bdd7e12ff6ff2ff7222db11
caio1213pgm <caiofxpgm@gmail.com>
2025-07-28 15:31:44 -0300

    fix: ensure async handling in addUserToFirestorage function for proper error management

--- src/hooks/useAddUserToFirestorage.tsx
+++ src/hooks/useAddUserToFirestorage.tsx
@@ -4,19 +4,19 @@ import { db } from "../service/firebase";
 import { doc, setDoc } from "firebase/firestore";
 
 async function addUserToFirestorage(data: FormRegisterData, user: User) {
-  try {
-    if (user !== undefined) {
-      const docUser = doc(db, "users", user.uid);
-      const docData = {
-        email: data.email,
-        username: data.username,
-        date: new Date().toLocaleDateString("pt-BR"),
-        id: user.uid,
-      };
-      setDoc(docUser, docData);
+    try {
+        if (user !== undefined) {
+            const docUser = doc(db, "users", user.uid);
+            const docData = {
+                email: data.email,
+                username: data.username,
+                date: new Date().toLocaleDateString("pt-BR"),
+                id: user.uid,
+            };
+            await setDoc(docUser, docData);
+        }
+    } catch (error) {
+        console.error("Erro ao adicionar usuário ao Firestore:", error);
     }
-  } catch (error) {
-    console.error("Erro ao adicionar usuário ao Firestore:", error);
-  }
 }
 export default addUserToFirestorage;
