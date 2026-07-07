579d32f01873b00e2dac8563f88758031f9e3a5a
caio1213pgm <caiofxpgm@gmail.com>
2026-07-06 21:29:42 -0300

    feat: refactor CardList and CardListMenu components for improved functionality and code organization

--- src/components/CardListMenu.tsx
+++ src/components/CardListMenu.tsx
@@ -1,32 +1,72 @@
-import { DropdownMenuLabel } from "@radix-ui/react-dropdown-menu";
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
-import ButtonCopy from "./buttons/ButtonCopy";
-import { EllipsisVertical } from "lucide-react";
+import { onCopyLink } from "@/utils/copyLink";
+import { Copy, EllipsisVertical, FileDown, Trash2 } from "lucide-react";
+import { useState } from "react";
 import DeleteListDialog from "./DeleteListDialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 function CardListMenu({ id }: { id: string }) {
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger className="p-1 cursor-pointer hover:bg-gray-700 bg-gray-800 rounded">
-                <EllipsisVertical />
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="p-4 ">
-                <DropdownMenuLabel className="flex justify-between items-center gap-2">
-                    Copiar link de inscrição
-                    <ButtonCopy listId={id} />
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuLabel className="flex justify-between items-center ">
-                    <p>Excluir Lista</p>
-                    <DeleteListDialog id={id} />
-                </DropdownMenuLabel>
-            </DropdownMenuContent>
-        </DropdownMenu>
-    );
+  const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+  const itensMenu: {
+    label: string;
+    icon: React.ReactElement;
+    onClick: () => void;
+    separator?: boolean;
+  }[] = [
+    {
+      icon: <Copy />,
+      label: "Copiar link",
+      onClick: () => {
+        onCopyLink({ listId: id });
+      },
+      separator: true,
+    },
+    {
+      icon: <Trash2 />,
+      label: "Apagar lista",
+      onClick: () => {
+        setOpenDeleteDialog(true);
+      },
+      separator: true,
+    },
+    { icon: <FileDown />, label: "Exportar .csv", onClick: () => {} },
+  ];
+  return (
+    <>
+      <DeleteListDialog
+        id={id}
+        open={openDeleteDialog}
+        onClose={() => {
+          setOpenDeleteDialog(false);
+        }}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger className="p-1 cursor-pointer hover:bg-gray-700 bg-gray-800 rounded">
+          <EllipsisVertical />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          className="px-4 py-2"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+        >
+          {itensMenu.map((item) => (
+            <>
+              <DropdownMenuItem onClick={item.onClick}>
+                {item.icon}
+                {item.label}
+              </DropdownMenuItem>
+              {item.separator && <DropdownMenuSeparator />}
+            </>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
 }
 export default CardListMenu;
